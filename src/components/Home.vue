3411d716ff4bc4fd156d45ff94fa178dd141a8fb
Abhishek Waghela <abhi27051995@gmail.com>
2018-09-16 13:49:20 +0530

    New TypeWriter component
    Changes Included
     - Added a new TypeWriter component
     - Used TypeWriter component to display company name
    
    Signed-off-by: Abhishek Waghela <abhi27051995@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,31 @@
 <template>
-
-    <h1>Welcome to Ashtonish Tech. Emerge Private LTD</h1>
-
+    <div class="p-4 p-xl-5 typeWriterContainer">
+        <TypeWriter :text="text" :speed="speed" :freezeTime="freezeTime"/>
+    </div>
 </template>
 
 <script>
+import TypeWriter from '@/components/TypeWriter'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    TypeWriter
+  },
+  data () {
+    return {
+      text: 'Welcome to Ashtonish Tech. Emerge Private Limited.',
+      speed: 100,
+      freezeTime: 5000
+    }
+  }
 }
 </script>
+
+<style>
+.typeWriterContainer {
+    color: #fff;
+    background-color: #000;
+    text-align: center;
+}
+</style>
